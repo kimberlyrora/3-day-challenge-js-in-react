72dc05c77eb0467c65c03b68786c335a6ace673f
kimberlyrojasra <kimberlyrojasra@gmail.com>
2019-06-21 17:51:15 -0500

    Updates

---
+++ precache-manifest.b0e118af8c1b45b547bcefd2b4a608d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9ee424c26d7073d3e965e0a9497c132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fb23b117d0e47734fe2",
+    "url": "/static/css/main.8d31ee6b.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "7fb23b117d0e47734fe2",
+    "url": "/static/js/main.c915e3dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
